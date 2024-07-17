9d260ea002fc968984cb2fcf4575e49488e0ebd5
Sourav Bisht <souravbishthere@gmail.com>
2024-07-18 00:08:27 +0530

    Update 6. Best Time to Buy and Sell Stock.cpp

--- 45-Days Challenge/17 July 2024/6. Best Time to Buy and Sell Stock.cpp
+++ 45-Days Challenge/17 July 2024/6. Best Time to Buy and Sell Stock.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int tempProfit = 0;
+        int n = prices.size();
+        int buyDate = prices[0];
+        int sellDate;
+        for( int i = 1; i<n; i++ )
+        {
+            sellDate = prices[i];
+            tempProfit = sellDate - buyDate;
+            if( tempProfit > profit ) { profit = tempProfit; }
 
+            if( tempProfit <= 0 )
+            {
+                buyDate = prices[i];
+            }
+        }
+    return profit;
+    }
+};
