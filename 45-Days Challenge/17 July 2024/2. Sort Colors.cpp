3a1e2142c02339201dad4df3a00f0f211c2e60c9
Sourav Bisht <souravbishthere@gmail.com>
2024-07-18 00:06:42 +0530

    Update 2. Sort Colors.cpp

--- 45-Days Challenge/17 July 2024/2. Sort Colors.cpp
+++ 45-Days Challenge/17 July 2024/2. Sort Colors.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int indZero = 0;
+        int indTwo = nums.size()-1;
+        int i = 0;
+        while( i <= indTwo )
+        {
+            if( nums[i] == 0 )
+            {
+                swap(nums[i],nums[indZero]);
+                ++indZero;
+                ++i;
+            }
+            else if( nums[i] == 2 )
+            {
+                swap( nums[i], nums[indTwo] );
+                --indTwo;
+            }
+            else ++i;
+        }
+    }
+};
