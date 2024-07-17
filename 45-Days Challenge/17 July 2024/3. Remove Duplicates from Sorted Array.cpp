60fde651c8a752d60db46e9f6b0800b9497e099d
Sourav Bisht <souravbishthere@gmail.com>
2024-07-18 00:07:25 +0530

    Update 3. Remove Duplicates from Sorted Array.cpp

--- 45-Days Challenge/17 July 2024/3. Remove Duplicates from Sorted Array.cpp
+++ 45-Days Challenge/17 July 2024/3. Remove Duplicates from Sorted Array.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 0;
+        int i = 1;
+        int n = nums.size();
 
+        while( i<n )
+        {
+            if( nums[k] != nums[i] )
+            {
+                nums[k+1] = nums[i];
+                k++;
+            }
+            i++;
+        }
+    return k+1;
+    }
+};
