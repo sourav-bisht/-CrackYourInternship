28ecb8e57c6fa92f5e911c95bb4b3d676df66e3b
Sourav Bisht <souravbishthere@gmail.com>
2024-07-21 02:12:29 +0530

    Update 21. Majority Element.cpp

--- 45-Days Challenge/20 July 2024/21. Majority Element.cpp
+++ 45-Days Challenge/20 July 2024/21. Majority Element.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        int ans;
 
+        for( int i = 0; i<n; i++ )
+        {
+            if( count == 0 )
+            {
+                ans = nums[i];
+                ++count;
+            }
+            else if( nums[i] != ans )
+            {
+                --count;
+            }
+            else ++count;
+        }
+    return ans;
+    }
+};
