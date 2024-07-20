ec65d5ca46874e491c933738c2a607689f94d0e0
Sourav Bisht <souravbishthere@gmail.com>
2024-07-20 12:40:40 +0530

    Update 15. Maximum points you can obtain from Cards.cpp

--- 45-Days Challenge/19 July 2024/15. Maximum points you can obtain from Cards.cpp
+++ 45-Days Challenge/19 July 2024/15. Maximum points you can obtain from Cards.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int leftSum = 0;
+        int rightSum = 0;
+        int sum = 0;
+        int j,l;
+        for( int i = 0; i<k; i++ )
+        {
+            leftSum += cardPoints[i];
+        }
+        sum = leftSum;
 
+        j = k-1;
+        l = cardPoints.size()-1;
+        while( j >= 0 )
+        {
+            leftSum -= cardPoints[j];
+            rightSum += cardPoints[l];
+            if( leftSum+rightSum > sum )
+            {
+                sum = leftSum + rightSum;
+            }
+        --j;
+        --l;
+        }
+    return sum;
+    }
+};
