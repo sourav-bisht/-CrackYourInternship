fcc8c6798c69aae1ece9c93d8c604bce2d5849cc
Sourav Bisht <souravbishthere@gmail.com>
2024-07-21 02:11:54 +0530

    Update 20. Jump Game.cpp

--- 45-Days Challenge/20 July 2024/20. Jump Game.cpp
+++ 45-Days Challenge/20 July 2024/20. Jump Game.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> dp(n,false);
+        dp[0] = true;
+        for( int i = 0; i<n; i++ )
+        {
+            if(dp[i])
+            {
+                int farthest_jump = min(i+nums[i],n-1);
+                for( int j = i+1; j<=farthest_jump; ++j )
+                {
+                    dp[j] = true;
+                }
+            }
+        }
+    return dp[n-1];
+    }
+};
