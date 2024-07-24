950cb301064f9e67c35a09f8044fa0b530fd1b61
Sourav Bisht <souravbishthere@gmail.com>
2024-07-25 01:03:23 +0530

    Update 37. Reverse Words in a string.cpp

--- 45-Days Challenge/24 July 2024/37. Reverse Words in a string.cpp
+++ 45-Days Challenge/24 July 2024/37. Reverse Words in a string.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans = "";
+        string temp = "";
+        int n = s.length();
+        for( int i = (n-1); i>=0; --i )
+        {
+            if( s[i] != ' ' )
+            {
+                temp = s[i] + temp;
+            }
+            if( s[i] == ' ' || i == 0 )
+            {a
+                ans = ans + temp;
+                temp = "";
+            }
+            if( !ans.empty() && ans.back() != ' ' )
+            {
+                ans.push_back(' ');
+            }
+        }
+        if( ans.back() == ' ') ans.pop_back();
+    return ans;
+    }
+};
